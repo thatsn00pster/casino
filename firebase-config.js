5276edc6465c6c4c942ab42d4e6b25346703e249
thatsn00pster <168500351+thatsn00pster@users.noreply.github.com>
2025-12-10 16:41:13 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLmhwz-ocTNaZwWchcpWl3N0hJMlsskts",
+  authDomain: "nebula-48dbd.firebaseapp.com",
+  databaseURL: "https://nebula-48dbd-default-rtdb.firebaseio.com",
+  projectId: "nebula-48dbd",
+  storageBucket: "nebula-48dbd.firebasestorage.app",
+  messagingSenderId: "259199297883",
+  appId: "1:259199297883:web:f84fe6b2fc2ca70015d27e",
+  measurementId: "G-38CB3Y2T56"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
